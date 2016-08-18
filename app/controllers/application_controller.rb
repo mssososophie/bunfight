964c4ce738e5747a75f9fd681550270eb88bba8a
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2016-08-18 18:04:05 +0100

    bun fight needs a 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	def not_found
+	  raise ActionController::RoutingError.new('Doh! Bread name not found')
+	end
+
 end
