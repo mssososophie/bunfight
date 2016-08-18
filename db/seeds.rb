c747d4474a4dccdeb585de492ef2457a8ae0cd0b
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2016-08-18 18:17:13 +0100

    New bread names added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 BreadName.transaction do 
 	BreadName.delete_all
+	# [
+	# 	["Manchester", "Barm"],
+	# 	["Liverpool", "Barmcake"],
+	# ].map{|city, bread|
+	# 	BreadName.create(city_name: city, bread_name: bread)	
+	# }
 	BreadName.create(city_name: "Manchester", bread_name: "Barm")
 	BreadName.create(city_name: "Liverpool", bread_name: "Barmcake")
+	BreadName.create(city_name: "Birmingham", bread_name: "Roll")
+	BreadName.create(city_name: "Whitehaven", bread_name: "Teacake")
+	BreadName.create(city_name: "Newcastle", bread_name: "Stottie or a Stottie cake")
+	BreadName.create(city_name: "Whitehaven", bread_name: "Teacake")
+	BreadName.create(city_name: "Leeds", bread_name: "Breadcake")
+	BreadName.create(city_name: "Coventry", bread_name: "Batch")
+	BreadName.create(city_name: "Bury", bread_name: "Teacake")
+	BreadName.create(city_name: "Belfast", bread_name: "Bap")
+	BreadName.create(city_name: "Southport", bread_name: "Barm")
+	BreadName.create(city_name: "Oxford", bread_name: "Roll")
+	BreadName.create(city_name: "Exeter", bread_name: "Bap")
+	BreadName.create(city_name: "Huddersfield", bread_name: "Teacake")
+	BreadName.create(city_name: "Middlesbrough", bread_name: "Bun")
+	BreadName.create(city_name: "London", bread_name: "Bun")
+	BreadName.create(city_name: "Glasgow", bread_name: "Piece")
+	BreadName.create(city_name: "Warrington", bread_name: "Bun")
+
 end
