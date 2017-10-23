bb073b50ef2270270c816814c2c06a47d1a5e36e
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2017-10-23 20:01:01 +0100

    change background to yellow

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,29 +2,32 @@ require "geoip"
 
 class SiteController < ApplicationController	
 	def index
-		@city_name = "Manchestaaaar"
-		@bread = "Muffin"
 
-		# remote_ip = request.remote_ip
-		# logger.debug remote_ip == "::1"
-		# if remote_ip == "::1" 
-		# 	remote_ip = "164.39.71.226"
-		# end
+		remote_ip = request.remote_ip
+		logger.debug remote_ip == "::1"
+		if remote_ip == "::1" 
+			remote_ip = "164.39.71.226"
+		end
 
-		# c = GeoIP.new('GeoLiteCity.dat').city(remote_ip)
-		# logger.debug c.inspect
+		c = GeoIP.new('GeoLiteCity.dat').city(remote_ip)
+		logger.debug c.inspect
 
-		# if c.nil? 
-		# 	#render plain: "No bread for you, chump!"
-		# 	not_found
-		# else
+		if c.nil? 
+			#render plain: "No bread for you, chump!"
+			not_found
+			return
+		end
 
-		# 	bn = BreadName.find_by(city_name: c.city_name)
+		bn = BreadName.find_by(city_name: c.city_name)
 
-		# 	@city_name = c.city_name
+		if bn.nil?
+			not_found
+			return
+		end
 
-		# 	@bread = bn.bread_name
-		# end
+		@city_name = c.city_name
+
+		@bread = bn.bread_name
 	end
 
 end
