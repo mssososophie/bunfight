71d2605f923b2e9f5a011784ac15e98eff75a3ba
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2017-10-23 19:44:58 +0100

    Prototype the site controller for debugger

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,26 +2,29 @@ require "geoip"
 
 class SiteController < ApplicationController	
 	def index
-		remote_ip = request.remote_ip
-		logger.debug remote_ip == "::1"
-		if remote_ip == "::1" 
-			remote_ip = "164.39.71.226"
-		end
+		@city_name = "Manchestaaaar"
+		@bread = "Muffin"
 
-		c = GeoIP.new('GeoLiteCity.dat').city(remote_ip)
-		logger.debug c.inspect
+		# remote_ip = request.remote_ip
+		# logger.debug remote_ip == "::1"
+		# if remote_ip == "::1" 
+		# 	remote_ip = "164.39.71.226"
+		# end
 
-		if c.nil? 
-			#render plain: "No bread for you, chump!"
-			not_found
-		else
+		# c = GeoIP.new('GeoLiteCity.dat').city(remote_ip)
+		# logger.debug c.inspect
 
-			bn = BreadName.find_by(city_name: c.city_name)
+		# if c.nil? 
+		# 	#render plain: "No bread for you, chump!"
+		# 	not_found
+		# else
 
-			@city_name = c.city_name
+		# 	bn = BreadName.find_by(city_name: c.city_name)
 
-			@bread = bn.bread_name
-		end
+		# 	@city_name = c.city_name
+
+		# 	@bread = bn.bread_name
+		# end
 	end
 
 end
