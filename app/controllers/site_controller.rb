dfeded1a47583e644a1e051988809385c2f69070
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2017-11-13 19:31:28 +0000

    Default to roll if bread name not found

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -10,18 +10,10 @@ class SiteController < ApplicationController
 			remote_ip = "164.39.71.226"
 		end
 
-		geoip_city = GeoIP.new('GeoLiteCity.dat').city(remote_ip)
+		geoip_city = GeoIP.new('GeoLiteCity.dat').city(remote_ip)g
 
-		city_name = geoip_city ? geoip_city.city_name : 'Bristol'
+		@city_name = geoip_city ? geoip_city.city_name : 'Bristol'
 
-		bn = BreadName.find_by(city_name: city_name)
-
-		if bn.nil?
-			not_found
-			return
-		end
-
-		@city_name = city_name
-		@bread = bn.bread_name
+		@bread_name = BreadName.find_by(city_name: city_name) || 'Roll'
 	end
 end
