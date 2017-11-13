f77dc8fe5d3c8eba868870306df0ff86f5c9340f
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2017-11-13 19:26:21 +0000

    Add ip debugging

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,8 @@ require "geoip"
 class SiteController < ApplicationController	
 	def index
 		remote_ip = request.remote_ip
+		logger.debug "remote ip was #{remote_ip}"
+
 		logger.debug remote_ip == "::1"
 		if remote_ip == "::1" 
 			remote_ip = "164.39.71.226"
